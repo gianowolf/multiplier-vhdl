b6404c95b73025d0defcfc41679c4b66b19a4d11
Gian Franco Lasala <gianfrancolasala@hotmail.com>
2020-11-27 03:38:29 -0300

    last commit

--- Multi4.vhd
+++ Multi4.vhd
@@ -22,8 +22,8 @@ ARCHITECTURE Structural of Multi4 is
 	
 	component Latch8
 	  port(
+	  	D            : IN  BIT_VECTOR(7 downto 0); 
 		CLK, PRE, CLR: IN  BIT;
-		D            : IN  BIT_VECTOR(7 downto 0); 
 		Q            : OUT BIT_VECTOR(7 downto 0)
 	  );
 	end component;	
@@ -55,21 +55,14 @@ begin
 	RB   : ShiftN     port map(CLK, '0', INIT, SHIFT, '1', inRB, outRB);  
 	SM   : Controller port map(STB, CLK, LSB, STOP, INIT, SHIFT, ADD, DONE);
 	ADDER: Adder8     port map(outRB, outACC, '0', Cout, SUM);
-	ACC  : Latch8     port map(CLK, '1', '1', inACC, outACC);
-	Rout : Latch8     port map(CLK, '1', '1', inR, RES);
+	ACC  : ShiftN     port map(CLK, INIT, ADD, '0', '0', SUM, outACC);
+	Rout : Latch8     port map(outACC, CLK, '1', '1', RES);  
 	
     -- Esta linea podria obviarse y pasar outRA(0) al port map del SM : Controller
-	-- Se utiliza para respetar el formato del multiplicado brindado por la catedra y facilitar su lectura
+	-- Se la utiliza para facilitar lectura
 	LSB <= outRA(0);
 	
 	-- NOR entre los bits del registro A.
 	-- Cuando todos los bits de A son 0, se avanza al STATE: STOP.
 	STOP <= not(outRA(7) or outRA(6) or outRA(5) or outRA(4)  or outRA(3) or outRA(2) or outRA(1) or outRA(0));
-	
-	-- Funciona como señal de LOAD para el Latch que recibe el resultado de la suma
-	inACC <= SUM when ADD'event and ADD='1';			 
-	
-	-- Funciona como señal de LOAD para el Latch Y. carga el resultado cuando se ha finalizado la operacion
-	inR <= outACC when STOP'event and STOP='1';
-	
 end;
--- TestExhaustivo.vhd
+++ TestExhaustivo.vhd
@@ -23,29 +23,31 @@ ARCHITECTURE Driver of TestMulti4 is
 	signal RES : BIT_VECTOR(7 downto 0);
 	
 	signal L : NATURAL := 4;  
-	signal Operando1: NATURAL;
-	signal Operando2: NATURAL;
+	signal Operando1 : NATURAL := 0;
+	signal Operando2 : NATURAL := 0;
 	signal Producto : NATURAL;
 
 begin 
 
     UUT: Multi4 port map(STB, CLK, inRA, inRB, DONE, RES);
 	
-	clock(CLK, 5ns , 5ns);
+	clock(CLK, 30ns , 30ns);
 
     Stimulus: process	
 	begin
 		for i in 0 to 15 loop 
-		inRA <= convert(i,L);
-		for j in 0 to 15 loop 	 
-			inRB <= convert(j,L);				
-			STB <= '1';	 
-			Operando1 <= j;	--facilitar lectura waveform	 
-			Operando2 <= i;	--facilitar lectura waveform
-			wait until DONE'EVENT and DONE='1';
-			Producto <= convert(RES); -- facilitar lectura waveform	 
+			inRA <= convert(i,L);
+			for j in 0 to 15 loop 
+				inRB <= convert(j,L);
+				Operando1 <= j;		 
+				Operando2 <= i;
+				STB <= '1';
+				wait for 20ns;
+				STB <= '0';
+				wait until DONE'EVENT and DONE='1'; 
+				Producto <= convert(RES);
+			end loop;
 		end loop;
-	end loop;
-	wait;
+		wait;
 	end process;
 end;
